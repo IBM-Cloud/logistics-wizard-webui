4e70d1207ca1bd6839d4fd7ab333a6c0e6353fe4
Colby Williams <colby@colbycheeze.com>
2016-09-02 10:24:48 -0500

    Add some config for Travis and Coveralls (#26)
    * Add some config for Travis and Coveralls
    * Updated node version in travis.yml
    * Tests now auto assign global from config

--- config/ava.config.js
+++ config/ava.config.js
@@ -2,6 +2,9 @@
 
 import { argv } from 'yargs';
 import fs from 'fs';
+import config from '../config';
+
+Object.assign(global, config.globals);
 
 // Needed to avoid errors with requiring css modules from js
 const mockCssModules = require('mock-css-modules');
@@ -25,16 +28,3 @@ require.extensions['.html'] = (module, filename) => {
 global.document = require('jsdom').jsdom(require('../src/index.html'));
 global.window = document.defaultView;
 global.navigator = window.navigator;
-
-// Set up project globals
-const env = process.env.NODE_ENV || 'development';
-global.process.env = {
-  'NODE_ENV' : JSON.stringify(env),
-};
-global['NODE_ENV'] = env;
-global['__DEV__'] = env === 'development';
-global['__PROD__'] = env === 'production';
-global['__TEST__'] = env === 'test';
-global['__DEBUG__'] = env === 'development' && !argv.no_debug;
-global['__COVERAGE__'] = !argv.watch && env === 'test';
-global['__BASENAME__'] = JSON.stringify(process.env.BASENAME || '');
