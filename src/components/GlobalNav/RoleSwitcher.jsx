0143099bb9f40b80fddac29ea9faa9d09c39a7bf
COLBY WILLIAMS <colby@colbycheeze.com>
2016-09-04 16:36:30 -0500

    Updated some styles in Global Nav

--- src/components/GlobalNav/RoleSwitcher.jsx
+++ src/components/GlobalNav/RoleSwitcher.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
 import IconMenu from 'material-ui/IconMenu';
-import { Icon } from 'react-fa';
+import IconButton from 'material-ui/IconButton';
+import AccountUser from 'material-ui/svg-icons/action/account-circle';
+import { palette } from 'styles/muiTheme';
 import RoleItem from './RoleItem';
-import classes from './GlobalNav.scss';
 
-// const styles = {
-//   stack: {
-//     position: 'relative',
-//     cursor: 'pointer',
-//   },
-//   user: {
-//     fontSize: 30,
-//     color: 'rgb(15, 147, 166)',
-//     lineHeight: '62px',
-//   },
-//   circle: {
-//     fontSize: 34,
-//     color: 'rgb(255, 255, 255)',
-//     lineHeight: '56px',
-//   },
-// };
+const iconStyles = {
+  width: '2rem',
+  height: '2rem',
+};
 
-// Props have been removed since they are not currently needed.
-// To add props back in, simply change the line below to: ...RoleSwitcher = (props) => (...
-export const RoleSwitcher = () => (
-  // TODO: Find out what's causing runtime error.
-  <IconMenu
-    iconButtonElement={
-      <span className={classes.stack}>
-        <Icon
-          stack="2x"
-          name="circle"
-          className={classes.circle}
-        />
-        <Icon
-          stack="1x"
-          name="user"
-          className={classes.user}
-        />
-      </span>
-    }
-  >
-    <RoleItem label="Supply Chain Manager" icon="fa-user" />
-    <RoleItem label="Retail Manager" location="Austin, Texas" icon="fa-user" selected />
-    <RoleItem label="Retail Manager" location="Chino, California" icon="fa-user" />
-    <RoleItem label="Create New Retail Manager" type="button" icon="fa-plus" />
-  </IconMenu>
+const AccountButton = () => (
+  <IconButton iconStyle={iconStyles}>
+    <AccountUser
+      color={palette.primary3Color}
+      hoverColor={palette.alternateTextColor}
+    />
+  </IconButton>
 );
+
+export const RoleSwitcher = ({ users, login, createUser }) => (
+  users
+  ? <IconMenu
+    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+    iconButtonElement={AccountButton()}
+  >
+    {users.map((user, key) => (
+      <RoleItem
+        key={key}
+        user={user}
+        roleAction={login}
+      />
+    ))}
+    <RoleItem roleAction={createUser} />
+  </IconMenu>
+  : <span>No Demo Session</span>
+);
+
+RoleSwitcher.propTypes = {
+  users: React.PropTypes.arrayOf(React.PropTypes.shape({
+    id: React.PropTypes.number.isRequired,
+    role: React.PropTypes.string.isRequired,
+    location: React.PropTypes.string,
+    loggedIn: React.PropTypes.bool,
+  }).isRequired),
+  login: React.PropTypes.func.isRequired,
+  createUser: React.PropTypes.func.isRequired,
+};
+
 export default RoleSwitcher;
