4aed68960c6c7f705930efa87361a941617be000
COLBY WILLIAMS <colby@colbycheeze.com>
2016-09-06 10:30:45 -0500

    WIP commit

--- src/components/GlobalNav/GlobalNav.test.js
+++ src/components/GlobalNav/GlobalNav.test.js
@@ -7,27 +7,16 @@ const setup = () => {
   const spies = {
   };
   const props = {
-    users: [
-      {
-        role: 'supplychainmanager',
-        loggedIn: true,
-      },
-      {
-        role: 'retailstoremanager',
-        location: 'Austin, TX',
-      },
-    ],
   };
-  const component = shallow(<GlobalNav {...props} />);
+  const component = shallow(<GlobalNav />);
 
   return { component, props, spies };
 };
 
 test('(Component) Has expected elements.', t => {
-  const { component, props } = setup();
+  const { component } = setup();
 
-  t.is(component.find('RoleSwitcher').length, 1);
-  t.deepEqual(component.find('RoleSwitcher').first().props().users, props.users);
+  t.is(component.find('Connect(RoleSwitcher)').length, 1);
   t.is(component.find('Link').length, 1);
   t.is(component.find('Link').first().props().to, '/');
 });
