0697d835e9c76b75f622f5dcc905eb92a202fc08
Ram Vennam <rvennam@users.noreply.github.com>
2016-12-13 20:56:37 -0600

    Display shipment recommendations #225 (#91)

--- src/routes/Dashboard/components/Map/PopUpCard/StormCard/StormCard.jsx
+++ src/routes/Dashboard/components/Map/PopUpCard/StormCard/StormCard.jsx
@@ -7,22 +7,50 @@ const StormCard = ({ storm }) => {
     recommendations,
   } = storm;
 
+
   return (
     <div className={classes.contentContainer}>
-      <div className={classes.subtitle}>
-        Type
+      <div className={classes.iconTitleContainer}>
+        <div className={classes.subtitle}>
+          <img className={classes.weatherIcon} role="presentation" src="../storm.png" />
+        </div>
+        <div>
+          <div>
+            <h2>{event.event_desc}</h2>
+          </div>
+
+          <span className={classes.subtitle}>
+        Severity:&nbsp;
+      </span>
+          {event.severity}
+        </div>
       </div>
-      <div>{event.event_desc}</div>
+
 
       <div className={classes.subtitle}>
-        Severity
+        Suggested Shipments
       </div>
-      <div>{event.severity}</div>
-
-      <div className={classes.subtitle}>
-        Recommendations
+      <div>
+        <small>
+          Potential supply shortages due to weather.
+          Consider sending additional supplies to affected locations.
+        </small>
+        {recommendations.map(recommendation =>
+          <div className={classes.shipmentDialog}>
+            <div className={classes.shipmentTitle}>
+              Shipment from {recommendation.fromId} to {recommendation.toId}
+            </div>
+            <div className={classes.shipmentDialogActionContainer}>
+              <div className={classes.shipmentDialogAction}>
+                Reject
+              </div>
+              <div className={classes.shipmentDialogAction}>
+                Approve
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      <div>{recommendations.length || '0'}</div>
     </div>
   );
 };
