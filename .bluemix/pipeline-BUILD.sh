186ef158d08b5e3d0c7221d8d6020ca96b6b5e26
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2016-12-14 16:07:44 +0100

    extract pipeline scripts to repo https://github.com/IBM-Bluemix/logistics-wizard-toolchain/issues/28

---
+++ .bluemix/pipeline-BUILD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+npm config delete prefix
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 4.4
+npm install
+npm run test
+npm run check-coverage
+if [ -z ${COVERALLS_REPO_TOKEN} ]; then
+  echo No Coveralls token specified, skipping coveralls.io upload
+else
+  COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN npm run coveralls
+fi
+# inject the location of the controller service
+domain=".mybluemix.net"
+case "${REGION_ID}" in
+  ibm:yp:eu-gb)
+    domain=".eu-gb.mybluemix.net"
+  ;;
+  ibm:yp:au-syd)
+  domain=".au-syd.mybluemix.net"
+  ;;
+esac
+export CONTROLLER_SERVICE=https://$CONTROLLER_SERVICE_APP_NAME$domain
+npm run deploy:prod
---
+++ .bluemix/pipeline-DEPLOY.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Push the app
+if ! cf app $CF_APP; then
+  cf push $CF_APP -n $CF_APP
+else
+  OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
+  rollback() {
+    set +e
+    if cf app $OLD_CF_APP; then
+      cf logs $CF_APP --recent
+      cf delete $CF_APP -f
+      cf rename $OLD_CF_APP $CF_APP
+    fi
+    exit 1
+  }
+  set -e
+  trap rollback ERR
+  cf rename $CF_APP $OLD_CF_APP
+  cf push $CF_APP -n $CF_APP
+  cf delete $OLD_CF_APP -f
+fi
