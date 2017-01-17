0ad8f2527dfce7969b5d2fa26cdab3b8933b0611
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-01-17 14:06:19 +0100

    check dc rendering

--- src/routes/Dashboard/components/Map/PopUpCard/PopUpCard.test.js
+++ src/routes/Dashboard/components/Map/PopUpCard/PopUpCard.test.js
@@ -15,3 +15,36 @@ test('(Component) Renders with expected elements', t => {
   const component = shallow(<PopUpCard {...props} />);
   t.true(component.is('div'), 'is wrapped by a div');
 });
+
+const propsForDc = {
+  dashboard: {
+    infoBox: {
+      type: 'distributionCenter',
+      data: {
+        id: 1,
+      },
+    },
+    shipments: [],
+    retailers: [],
+    'distribution-centers': [
+      {
+        contact: {
+          name: 'Joseph Smith',
+        },
+        id: 1,
+        address: {
+          state: 'Utah',
+          city: 'Salt Lake City',
+          country: 'US',
+          latitude: 40.71,
+          longitude: -111.9,
+        },
+      },
+    ],
+  },
+  selectMarker: () => {},
+};
+test('(Component) Renders a DC card', t => {
+  const component = shallow(<PopUpCard {...propsForDc} />);
+  t.is(component.find('DCCard').length, 1, 'has a DC Card');
+});
