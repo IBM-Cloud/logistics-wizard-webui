cd43204eb0113748a33d08291caf340f09754796
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-01-26 11:51:55 +0100

    use simple Divs to hide/show map and shipments

--- src/routes/Dashboard/components/Dashboard.jsx
+++ src/routes/Dashboard/components/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Tabs, Tab } from 'material-ui/Tabs';
 import RaisedButton from 'material-ui/RaisedButton';
 import Map from './Map';
 import ShipmentsTable from './ShipmentsTable';
@@ -11,46 +10,16 @@ import DCSummaryCard from './DCSummaryCard';
 import AlertsCard from './AlertsCard';
 import classes from './Dashboard.scss';
 
-/**
- * Use our own tab template to hide the map using flexGrow = 0,
- * and not only the height. Failure to do so would break the map display
- * as the GoogleMap component needs a real height at some point.
- */
-const styles = {
-  width: '100%',
-  position: 'relative',
-  textAlign: 'initial',
+const visibleTab = {
+  display: 'flex',
+  flexGrow: 1,
 };
 
-// const visibleTab = {
-//   display: 'flex',
-//   flexGrow: 1,
-// };
-//
-// const invisibleTab = {
-//   display: 'flex',
-//   flexGrow: 0,
-//   height: 0,
-//   overflow: 'hidden',
-// };
-
-const TabTemplate = ({ children, selected, style }) => {
-  const templateStyle = Object.assign({}, styles, style);
-  if (!selected) {
-    templateStyle.flexGrow = 0;
-    templateStyle.height = 0;
-    templateStyle.overflow = 'hidden';
-  }
-  return (
-    <div className={classes.tabContainer} style={templateStyle}>
-      {children}
-    </div>
-  );
-};
-TabTemplate.propTypes = {
-  children: React.PropTypes.node,
-  selected: React.PropTypes.bool,
-  style: React.PropTypes.object,
+const invisibleTab = {
+  display: 'flex',
+  flexGrow: 0,
+  height: 0,
+  overflow: 'hidden',
 };
 
 class Dashboard extends React.PureComponent {
@@ -58,7 +27,7 @@ class Dashboard extends React.PureComponent {
   constructor(props) {
     super(props);
     this.setMapMode = this.setMapMode.bind(this);
-    this.setTableMode = this.setTableMode.bind(this);
+    this.setListMode = this.setListMode.bind(this);
   }
 
   componentWillMount() {
@@ -69,8 +38,8 @@ class Dashboard extends React.PureComponent {
     this.setState({ viewMode: 'map' });
   }
 
-  setTableMode() {
-    this.setState({ viewMode: 'table' });
+  setListMode() {
+    this.setState({ viewMode: 'list' });
   }
 
   render() {
@@ -84,31 +53,16 @@ class Dashboard extends React.PureComponent {
         </div>
         <AlertsCard />
         <div className={classes.viewMode}>
-          <RaisedButton label="Map" onClick={this.setMapMode} />
-          <RaisedButton label="Table" onClick={this.setTableMode} />
+          <span style={{ flexGrow: 1 }} />
+          <RaisedButton disabled={this.state.viewMode === 'map'} label="Map" onClick={this.setMapMode} />
+          <RaisedButton disabled={this.state.viewMode === 'list'} label="List" onClick={this.setListMode} />
         </div>
-        {/* <div style={this.state.viewMode === 'map' ? visibleTab : invisibleTab} >
+        <div style={this.state.viewMode === 'map' ? visibleTab : invisibleTab} >
           <Map />
         </div>
-        <div style={this.state.viewMode === 'table' ? visibleTab : invisibleTab} >
+        <div style={this.state.viewMode === 'list' ? visibleTab : invisibleTab} >
           <ShipmentsTable />
-        </div> */}
-
-        <Tabs
-          className={classes.tabs}
-          tabItemContainerStyle={{ display: 'none' }}
-          contentContainerClassName={classes.tab}
-          inkBarStyle={{ display: 'none' }}
-          value={this.state.viewMode}
-          tabTemplate={TabTemplate}
-        >
-          <Tab label="Map" value="map">
-            <Map />
-          </Tab>
-          <Tab label="Table" value="table">
-            <ShipmentsTable />
-          </Tab>
-        </Tabs>
+        </div>
       </div>
     );
   }
