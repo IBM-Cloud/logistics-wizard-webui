6f7b94c4baf11fb916ceb54cb8b83c79bdd2c15d
Ram Vennam <rvennam987@gmail.com>
2017-02-14 22:33:18 -0600

    google analytics

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,17 +1,24 @@
 import React, { PropTypes } from 'react';
+import ReactGA from 'react-ga';
 import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from 'styles/muiTheme';
 
+ReactGA.initialize('UA-92026109-1'); // Unique Google Analytics tracking number
+
 const AppContainer = (props) => {
   const { history, routes, store } = props;
 
+  function logPageView() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
   return (
     <Provider store={store}>
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={{ display: 'flex', flexGrow: 1, flexDirection: 'column' }}>
-          <Router history={history} children={routes} />
+          <Router history={history} children={routes} onUpdate={logPageView} />
         </div>
       </MuiThemeProvider>
     </Provider>
