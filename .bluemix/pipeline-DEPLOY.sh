7b800505d2c64a35564011a1f6f1fe805e416942
Van Staub <van_staub@us.ibm.com>
2018-06-22 14:20:28 -0400

    update cf commands

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Push the app
-if ! cf app $CF_APP; then
-  cf push $CF_APP -n $CF_APP
+if ! bx app show $CF_APP; then
+  bx app push $CF_APP -n $CF_APP
 else
   OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
   rollback() {
     set +e
-    if cf app $OLD_CF_APP; then
-      cf logs $CF_APP --recent
-      cf delete $CF_APP -f
-      cf rename $OLD_CF_APP $CF_APP
+    if bx app show $OLD_CF_APP; then
+      bx app logs $CF_APP --recent
+      bx app delete $CF_APP -f
+      bx app rename $OLD_CF_APP $CF_APP
     fi
     exit 1
   }
   set -e
   trap rollback ERR
-  cf rename $CF_APP $OLD_CF_APP
-  cf push $CF_APP -n $CF_APP
-  cf delete $OLD_CF_APP -f
+  bx app rename $CF_APP $OLD_CF_APP
+  bx app push $CF_APP -n $CF_APP
+  bx app delete $OLD_CF_APP -f
 fi
