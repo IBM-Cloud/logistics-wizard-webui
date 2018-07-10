020483d34374d2e0b98ce6446f1c938b7bf00182
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2018-07-10 11:52:39 +0200

    removed ref to wiki

--- src/routes/Home/components/LandingPage/Footer/Footer.jsx
+++ src/routes/Home/components/LandingPage/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import classes from './Footer.scss';
 export const Footer = () => (
   <div className={classes.footer}>
     <div className={classes.gradient} />
-    <h4>Learn more on the Logistics Wizard Wiki</h4>
-    <a href="https://github.com/IBM-Cloud/logistics-wizard/wiki">
-      <GhostButton label="Github Wiki" />
+    <h4>Learn more on the Logistics Wizard repository</h4>
+    <a href="https://github.com/IBM-Cloud/logistics-wizard">
+      <GhostButton label="Github" />
     </a>
   </div>
 );
